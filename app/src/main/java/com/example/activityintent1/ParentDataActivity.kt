f07e0f5582e7e86b181a5b426666577d72fb2dbf
rezarinaldi <rez4rinaldi@gmail.com>
2020-09-30 14:27:32 +0700

    tambah parent and school origin activity

---
+++ app/src/main/java/com/example/activityintent1/ParentDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activityintent1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParentDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parent_data)
+    }
+}
---
+++ app/src/main/java/com/example/activityintent1/SchoolDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activityintent1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SchoolDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_school_data)
+    }
+}
