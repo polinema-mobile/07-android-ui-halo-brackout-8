adac5c893ddba3e87139f69ec06d99e3e74bbc75
rezarinaldi <rez4rinaldi@gmail.com>
2020-10-05 09:31:26 +0700

    Menambahkan fitur Dark Mode

--- app/src/main/java/com/example/activityintent1/MainActivity.kt
+++ app/src/main/java/com/example/activityintent1/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.activityintent1
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.activityintent1.dataparcelize.PersonalData
 import java.util.*
 
@@ -79,6 +82,20 @@ class MainActivity : AppCompatActivity() {
         btnPersonalToParent.setOnClickListener {
             intentToParent()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add(Menu.NONE, 1 , Menu.NONE, "Light Mode")
+        menu?.add(Menu.NONE, 2 , Menu.NONE, "Dark Mode")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setUp() {
